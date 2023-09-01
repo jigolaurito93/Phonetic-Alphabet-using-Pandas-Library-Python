7bf4b080a0c83457b50a4b38e883d2e8a1420ca0
Jose Laurito <jigolaurito93@yahoo.com>
2023-09-01 04:16:38 -0500

    used ASCII art to display logo

--- main.py
+++ main.py
@@ -1,16 +1,19 @@
 import pandas
+from logo_art import logo
+
 
 alphabet_data = pandas.read_csv("phonetic_alphabet.csv")
 
-
+print(logo)
 # TODO 1: Create a dictionary in the format of Leter as key and phonetic word as value
 # Use list comprehension
 
 phonetic_dict = {row.letter:row.code for (index, row) in alphabet_data.iterrows()}
 
-print(phonetic_dict)
-
-
 # TODO 2: Create a list of the phonetic code words from a word the user inputs
 
+name_ = input("Enter a word: ").upper()
 
+
+phonetic_list = [phonetic_dict[letter] for letter in name_]
+print(phonetic_list)
