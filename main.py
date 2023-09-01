ad27880c1d6ed52dde79826572daf7c0712648a4
Jose Laurito <jigolaurito93@yahoo.com>
2023-09-01 04:02:09 -0500

    create to do list

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
 import pandas
 
 alphabet_data = pandas.read_csv("phonetic_alphabet.csv")
-print(alphabet_data)
+
+
+# TODO 1: Create a dictionary in the format of Leter as key and phonetic word as value
+# Use list comprehension
+
+
+
+
+# TODO 2: Create a list of the phonetic code words from a word the user inputs
