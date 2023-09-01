a79f7b8e4eac072b15f476d90c75bdbdfcf3b7ee
Jose Laurito <jigolaurito93@yahoo.com>
2023-09-01 04:06:37 -0500

    converted dataframe to dictionary with list comprehension for loop

--- main.py
+++ main.py
@@ -6,7 +6,11 @@ alphabet_data = pandas.read_csv("phonetic_alphabet.csv")
 # TODO 1: Create a dictionary in the format of Leter as key and phonetic word as value
 # Use list comprehension
 
+phonetic_dict = {row.letter:row.code for (index, row) in alphabet_data.iterrows()}
 
+print(phonetic_dict)
 
 
 # TODO 2: Create a list of the phonetic code words from a word the user inputs
+
+
